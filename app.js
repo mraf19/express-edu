be06ba3ee332d0d0f5108ffcd4479c9dcf4efffd
mraf19 <raflimr1902@gmail.com>
2021-12-27 09:37:55 +0700

    give some edit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const mainRouter = require("./router");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
